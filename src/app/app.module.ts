bbd5dfaff4f6061dbe02a620c001f95ea0710ee5
Sabari-1234 <sabarinathan5802@gmail.com>
2024-03-14 15:22:35 +0530

    google map api used successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule, GoogleMapsModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css',
+})
+export class MapComponent {
+  myLatLng = { lat: 48.829677, lng: 2.239609 }; // Map Options
+  mapOptions: google.maps.MapOptions = {
+    zoom: 1000,
+  };
+
+  markerOptions: google.maps.MarkerOptions = {};
+
+  spots: { id: number; lat: number; lng: number }[] = [
+    { id: 1, lat: 48.85952222328431, lng: 2.3347153257887454 },
+    { id: 2, lat: 48.80528296155103, lng: 2.2111191343824954 },
+    { id: 3, lat: 48.63132261107716, lng: 2.4308456968824954 },
+    { id: 4, lat: 48.77633134372322, lng: 2.4665512632887454 },
+    { id: 5, lat: 48.7871901580939, lng: 2.3127426695387454 },
+    { id: 6, lat: 48.829677, lng: 2.239609 },
+  ];
+}
